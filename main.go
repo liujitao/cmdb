28f9c6d9b2df1665b13ab91d2e2e64a1302ab762
liujitao <liujitao79@gmail.com>
2020-12-04 11:06:06 +0800

    修改格式

--- main.go
+++ main.go
@@ -1,105 +1,105 @@
 package main
 
 import (
-	"context"
-	"log"
-	"math/rand"
-	"strconv"
-	"time"
+    "context"
+    "log"
+    "math/rand"
+    "strconv"
+    "time"
 
-	"cmdb/common"
-	"cmdb/team"
-	"cmdb/user"
+    "cmdb/common"
+    "cmdb/team"
+    "cmdb/user"
 
-	"github.com/gin-gonic/gin"
-	"github.com/gin-gonic/gin/binding"
-	"go.mongodb.org/mongo-driver/bson/primitive"
-	"go.mongodb.org/mongo-driver/mongo"
+    "github.com/gin-gonic/gin"
+    "github.com/gin-gonic/gin/binding"
+    "go.mongodb.org/mongo-driver/bson/primitive"
+    "go.mongodb.org/mongo-driver/mongo"
 )
 
 func main() {
-	// 初始化数据库
-	client, err := common.InitMgoClient("mongodb://127.0.0.1:27017", "cmdb", 128)
-	if err != nil {
-		log.Println(err)
-	}
-	defer client.Disconnect(context.Background())
+    // 初始化数据库
+    client, err := common.InitMgoClient("mongodb://127.0.0.1:27017", "cmdb", 128)
+    if err != nil {
+        log.Println(err)
+    }
+    defer client.Disconnect(context.Background())
 
-	// 初始化redis
-	redis := common.InitRedisClient("127.0.0.1:6379", 0, 10)
-	defer redis.Close()
+    // 初始化redis
+    redis := common.InitRedisClient("127.0.0.1:6379", 0, 10)
+    defer redis.Close()
 
-	// 自定义校验器
-	binding.Validator = new(common.DefaultValidator)
+    // 自定义校验器
+    binding.Validator = new(common.DefaultValidator)
 
-	// 初始化gin
-	//route := gin.Default()
-	route := gin.New()
+    // 初始化gin
+    //route := gin.Default()
+    route := gin.New()
 
-	// 注册请求路径
-	v1 := route.Group("/api/v1")
-	user.UserLogin(v1.Group("/user"))
+    // 注册请求路径
+    v1 := route.Group("/api/v1")
+    user.UserLogin(v1.Group("/user"))
 
-	// 调用认证中间件
-	v1.Use(user.AuthMiddleware())
-	user.UserEndpoints(v1.Group("/user"))
-	team.TeamEndpoints(v1.Group("/team"))
+    // 调用认证中间件
+    v1.Use(user.AuthMiddleware())
+    user.UserEndpoints(v1.Group("/user"))
+    team.TeamEndpoints(v1.Group("/team"))
 
-	// 初始化数据
-	// initData(client)
+    // 初始化数据
+    // initData(client)
 
-	// 启动服务
-	if err := route.Run(":8000"); err != nil {
-		panic(err)
-	}
+    // 启动服务
+    if err := route.Run(":8000"); err != nil {
+        panic(err)
+    }
 }
 
 func initData(client *mongo.Client) {
-	var teams []interface{}
-	var users []interface{}
-	var ids []primitive.ObjectID
+    var teams []interface{}
+    var users []interface{}
+    var ids []primitive.ObjectID
 
-	var _id primitive.ObjectID
+    var _id primitive.ObjectID
 
-	db := client.Database("cmdb")
+    db := client.Database("cmdb")
 
-	for _, name := range []string{"未分组", "研发", "测试", "运维"} {
-		_id = primitive.NewObjectID()
-		ids = append(ids, _id)
-		teams = append(teams, team.Team{ID: _id, TeamName: name, CreateAt: time.Now().Local().Unix()})
+    for _, name := range []string{"未分组", "研发", "测试", "运维"} {
+        _id = primitive.NewObjectID()
+        ids = append(ids, _id)
+        teams = append(teams, team.Team{ID: _id, TeamName: name, CreateAt: time.Now().Local().Unix()})
 
-	}
+    }
 
-	if _, err := db.Collection("team").InsertMany(context.Background(), teams); err != nil {
-		log.Panicln(err)
-	}
+    if _, err := db.Collection("team").InsertMany(context.Background(), teams); err != nil {
+        log.Panicln(err)
+    }
 
-	users = append(users, user.User{
-		ID:       primitive.NewObjectID(),
-		UserName: "admin",
-		RealName: "管理员",
-		Mobile:   "00000000000",
-		Email:    "admin@abc.com",
-		Password: "admin",
-		CreateAt: time.Now().Local().Unix(),
-		Team:     ids[0],
-	})
+    users = append(users, user.User{
+        ID:       primitive.NewObjectID(),
+        UserName: "admin",
+        RealName: "管理员",
+        Mobile:   "00000000000",
+        Email:    "admin@abc.com",
+        Password: "admin",
+        CreateAt: time.Now().Local().Unix(),
+        Team:     ids[0],
+    })
 
-	for i := 10; i < 40; i++ {
-		id := strconv.Itoa(i)
-		users = append(users, user.User{
-			ID:       primitive.NewObjectID(),
-			UserName: "user" + id,
-			RealName: "用户" + id,
-			Mobile:   "139000000" + id,
-			Email:    "user" + id + "@abc.com",
-			Password: user.SetPassword("123456"),
-			CreateAt: time.Now().Local().Unix(),
-			Team:     ids[rand.Intn(3)],
-		})
-	}
+    for i := 10; i < 40; i++ {
+        id := strconv.Itoa(i)
+        users = append(users, user.User{
+            ID:       primitive.NewObjectID(),
+            UserName: "user" + id,
+            RealName: "用户" + id,
+            Mobile:   "139000000" + id,
+            Email:    "user" + id + "@abc.com",
+            Password: user.SetPassword("123456"),
+            CreateAt: time.Now().Local().Unix(),
+            Team:     ids[rand.Intn(3)],
+        })
+    }
 
-	if _, err := db.Collection("user").InsertMany(context.Background(), users); err != nil {
-		log.Panicln(err)
-	}
+    if _, err := db.Collection("user").InsertMany(context.Background(), users); err != nil {
+        log.Panicln(err)
+    }
 }
--- team/model.go
+++ team/model.go
@@ -1,33 +1,33 @@
 package team
 
 import (
-	"cmdb/common"
+    "cmdb/common"
 
-	"go.mongodb.org/mongo-driver/bson/primitive"
+    "go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 type Team struct {
-	ID       primitive.ObjectID `bson:"_id,omitempty" json:"_id"`
-	TeamName string             `bson:"team_name" json:"team_name"`
-	CreateAt int64              `bson:"create_at" json:"create_at"`
-	UpdateAt int64              `bson:"update_at" json:"update_at"`
+    ID       primitive.ObjectID `bson:"_id,omitempty" json:"_id"`
+    TeamName string             `bson:"team_name" json:"team_name"`
+    CreateAt int64              `bson:"create_at" json:"create_at"`
+    UpdateAt int64              `bson:"update_at" json:"update_at"`
 }
 
 type teamModel struct {
-	Mgo common.Mgo
+    Mgo common.Mgo
 }
 
 var TeamModel = newTeamModel()
 
 // 初始化
 func newTeamModel() *teamModel {
-	m := new(teamModel)
-	m.Mgo.SetCollName("team")
-	return m
+    m := new(teamModel)
+    m.Mgo.SetCollName("team")
+    return m
 }
 
 // 创建索引
 func CreateTeamIndex() {
-	TeamModel.Mgo.CreateIndex(map[string]int{"team_name": 1}, true)
-	TeamModel.Mgo.CreateIndex(map[string]int{"create_at": 1}, false)
+    TeamModel.Mgo.CreateIndex(map[string]int{"team_name": 1}, true)
+    TeamModel.Mgo.CreateIndex(map[string]int{"create_at": 1}, false)
 }
